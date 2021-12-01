f86e0a4d4e4dbe297db1a50310dade509abee59d
unknown <brahim.ezzali@hotmail.com>
2021-12-01 12:14:54 +0100

    make Exeption retun a dynamique message and add TransactionExeption

--- src/main/java/ma/octo/assignement/web/common/ExceptionHandelingController.java
+++ src/main/java/ma/octo/assignement/web/common/ExceptionHandelingController.java
@@ -8,16 +8,24 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import ma.octo.assignement.exceptions.TransactionException;
+
+
 @ControllerAdvice
 public class ExceptionHandelingController {
 
     @ExceptionHandler(SoldeDisponibleInsuffisantException.class)
     public ResponseEntity<String> handleSoldeDisponibleInsuffisantException(Exception ex, WebRequest request) {
-        return new ResponseEntity<>("Pas de solde pas de virement", null, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+        return new ResponseEntity<>(ex.getMessage(), null, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CompteNonExistantException.class)
     public ResponseEntity<String> handleCompteNonExistantException(Exception ex, WebRequest request) {
-        return new ResponseEntity<>("Compte introuvable", null, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(ex.getMessage(), null, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TransactionException.class)
+    public ResponseEntity<String> handleTransactionException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), null, HttpStatus.BAD_REQUEST);
     }
 }
